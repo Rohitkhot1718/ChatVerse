72fa68ec1fce4eaf113726db7de24e30a94bb112
Rohit Khot <rohitkhot414@gmail.com>
2025-06-04 21:46:49 +0530

    Fixed: Mongodb URI

--- Backend/utils/config.js
+++ Backend/utils/config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function MongoDBConnection() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/ChatVerse')
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('Mongodb connected...');
     } catch (error) {
         console.log('MongoDB Connection Error:', error);
